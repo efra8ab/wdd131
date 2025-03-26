e3d65e0f8f47eebf5c7ecc4f57319a84e7ce6dd8
efra8ab <efra8ab@gmail.com>
2025-03-26 16:33:52 -0600

    Handling Events

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,11 +3,28 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("ul");
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+button.addEventListener("click", function () {
+    if (input.value.trim() === "")
+    {
+        input.focus();
+        return;
+    }
 
-li.textContent = input.value;
-deleteButton.textContent = "X";
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+        
+    li.textContent = input.value;
+    deleteButton.textContent = "X";
 
-li.append(deleteButton);
-list.append(li);
+    deleteButton.addEventListener("click", function () {
+        list.removeChild(li);
+        input.focus();
+    });
+
+    li.append(deleteButton);
+    list.append(li);
+
+    input.value = "";
+    input.focus();
+    
+});

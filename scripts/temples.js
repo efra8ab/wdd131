d1673a0d4d7f90b788188a2dbe1df8d2f79936e2
efra8ab <efra8ab@gmail.com>
2025-03-29 17:42:47 -0600

    corrections to css

---
+++ scripts/temples.js
@@ -0,0 +1,8 @@
+const mainnav = document.querySelector(".navegation");
+const hamb = document.querySelector("#menu");
+
+hamb.addEventListener("click", () => {
+
+    mainnav.addEventListener("show");
+    hamb.addEventListener("show");
+})
